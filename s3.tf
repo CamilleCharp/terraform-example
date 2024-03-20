6083daa0b81e474910a3ae7b28ae673c9f745243
Camille Zerrouk <camille.charpentier77@gmail.com>
2024-03-20 09:47:31 +0100

    Added IAM config allowing writing to bucket

--- s3.tf
+++ s3.tf
@@ -29,3 +29,22 @@ resource "aws_s3_bucket_policy" "allow_bucket_reading" {
     ]
   })
 }
+
+resource "aws_s3_bucket_policy" "allow_bucket_writing" {
+  bucket = aws_s3_bucket.static-resources.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [{
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${aws_iam_role.bucket_writer.name}"
+      },
+      "Action": "s3:*",
+      "Resource": [
+        aws_s3_bucket.static-resources.arn,
+        "${aws_s3_bucket.static-resources.arn}/*"
+      ]
+    }]
+  })
+}
